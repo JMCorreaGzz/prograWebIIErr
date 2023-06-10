e394d5529c4f7db7382e9cca1321071e48633337
JMCorreaGzz <42364228+JMCorreaGzz@users.noreply.github.com>
2023-06-10 17:33:12 -0600

    Proyecto Final
    The End

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { LoginRegister } from './LoginRegister';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from './Register';
+import Tablero from './Tablero';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +13,7 @@ root.render(
       <Routes>
         <Route path="/" element={<LoginRegister />} />
         <Route path='/registro' element={<Register/>}></Route>
+        <Route path='/tablero' element={<Tablero/>}></Route>
       </Routes>
     </BrowserRouter>   
 );
